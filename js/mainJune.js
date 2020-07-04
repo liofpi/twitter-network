cf6241ff674b26beaf4340c85fae882ec62ecac2
liofpi <neoh.hup.eu@gmail.com>
2020-07-04 14:16:56 +0800

    change (for the fourth time) the config file URLs in main js files to include 'twitter-network' domain

--- js/mainJune.js
+++ js/mainJune.js
@@ -19,7 +19,7 @@ function GetQueryStringParams(sParam,defaultVal) {
 }
 
 
-jQuery.getJSON(GetQueryStringParams("config","/config/config.json"), function(data, textStatus, jqXHR) {
+jQuery.getJSON(GetQueryStringParams("config","config/config.json"), function(data, textStatus, jqXHR) {
 	config=data;
 	
 	if (config.type!="network") {
