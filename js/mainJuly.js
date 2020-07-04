4250b85001e79d3bb3e21159d9ecf47d398aba6b
liofpi <neoh.hup.eu@gmail.com>
2020-07-04 13:47:23 +0800

    restructure the file directory

--- js/mainJuly.js
+++ js/mainJuly.js
@@ -19,7 +19,7 @@ function GetQueryStringParams(sParam,defaultVal) {
 }
 
 
-jQuery.getJSON(GetQueryStringParams("config","configJuly.json"), function(data, textStatus, jqXHR) {
+jQuery.getJSON(GetQueryStringParams("config","../config/configJuly.json"), function(data, textStatus, jqXHR) {
 	config=data;
 	
 	if (config.type!="network") {
